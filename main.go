24bcf64b3ab337e61d43e324e8a5b036dec3fd5a
Sondre Gunneng <sondre.gunneng@proximahydrotech.no>
2025-03-11 13:18:41 +0100

    lesson fileserver

--- main.go
+++ main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+)
+
+const PORT = "8080"
+
+func main() {
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    ":" + (PORT),
+		Handler: mux,
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	log.Printf("Started listening on :%s\n", PORT)
+	log.Fatal(srv.ListenAndServe())
+}
