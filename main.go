4708af732fb0066624fef3686e6daf5faec89088
Sondre Gunneng <sondre.gunneng@proximahydrotech.no>
2025-03-12 14:25:59 +0100

    refactor

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -28,14 +30,77 @@ func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 func (a *apiConfig) showHits(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	res := fmt.Sprintf("Hits: %d", a.fileServerHits.Load())
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	res := fmt.Sprintf(`<html>
+	  <body>
+	    <h1>Welcome, Chirpy Admin</h1>
+	    <p>Chirpy has been visited %d times!</p>
+	  </body>
+	</html>`, a.fileServerHits.Load())
+
 	w.Write([]byte(res))
 }
 
 func (a *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
 	log.Println("Reset hits to 0")
 	a.fileServerHits.Store(0)
+}
+
+func validateChirp(w http.ResponseWriter, r *http.Request) {
+	type params struct {
+		Body string `json:"body"`
+	}
+
+	data := &params{}
+	raw := json.NewDecoder(r.Body)
+	raw.Decode(data)
+
+	if len(data.Body) > 140 {
+		respondWithError(w, http.StatusBadRequest, "Chirping too much, max is 140 characters")
+		return
+	}
+
+	result := profanityFilter(data.Body)
+
+	respondWithJSON(w, http.StatusOK, struct {
+		Clean string `json:"cleaned_body"`
+	}{Clean: result})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.WriteHeader(code)
+	d, _ := json.Marshal(struct {
+		Error string `json:"error"`
+	}{Error: msg})
+
+	w.Write(d)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	d, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("%v", err)
+		return
+	}
+
+	w.Header().Add("Content-Type", "text/json")
+	w.WriteHeader(code)
+	w.Write(d)
+}
+
+func profanityFilter(text string) string {
+	bannedWords := []string{"kerfuffle", "sharbert", "fornax"}
+	allWords := strings.Split(text, " ")
+
+	for i, word := range allWords {
+		for _, bword := range bannedWords {
+			if strings.ToUpper(word) == strings.ToUpper(bword) {
+				allWords[i] = "****"
+			}
+		}
+	}
+
+	return strings.Join(allWords, " ")
 }
 
 func main() {
@@ -49,15 +114,16 @@ func main() {
 	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	mux.Handle("/app/", api.middlewareMetricsInc(fileServerHandler))
 
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+	mux.HandleFunc("POST /api/validate_chirp", validateChirp)
 
 	// Metrics
-	mux.HandleFunc("GET /metrics", api.showHits)
-	mux.HandleFunc("GET /reset", api.resetHits)
+	mux.HandleFunc("GET /admin/metrics", api.showHits)
+	mux.HandleFunc("POST /admin/reset", api.resetHits)
 
 	log.Printf("Started listening on :%s\n", PORT)
 	log.Fatal(srv.ListenAndServe())
