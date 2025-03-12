8a206dd645beed5996b49bf8c928a67c4aa30c7d
Sondre Gunneng <sondre.gunneng@proximahydrotech.no>
2025-03-12 08:23:28 +0100

    middleware

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const PORT = "8080"
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func NewApiConfig() *apiConfig {
+	return &apiConfig{}
+}
+
+func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	a.fileServerHits.Add(1)
+	return next
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -13,8 +28,28 @@ func main() {
 		Addr:    ":" + (PORT),
 		Handler: mux,
 	}
+	apiCfg := NewApiConfig()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+
+	mux.Handle("GET /app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		res := fmt.Sprintf("Hits: %d", apiCfg.fileServerHits.Load())
+		w.Write([]byte(res))
+	})
+
+	mux.HandleFunc("GET /reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileServerHits.CompareAndSwap(0, 0)
+	})
 
 	log.Printf("Started listening on :%s\n", PORT)
 	log.Fatal(srv.ListenAndServe())
